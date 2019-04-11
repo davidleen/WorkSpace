95381932776fc0a9cbf2724617f4aa3e946ec7d5
林太伟 <davidleen29@gmail.com>
2019-04-11 23:37:30 +0800

    代码修改

---
+++ ReaderServer/library/src/main/java/com/giants3/reader/entity/Chapter.java
@@ -0,0 +1,27 @@
+package com.giants3.reader.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by davidleen29 on 2018/5/12.
+ */
+@Entity(name="T_Chapter")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="type", discriminatorType=DiscriminatorType.INTEGER)
+@DiscriminatorValue("0")
+public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+
+    public String name;
+
+    public long bookId;
+
+
+    public String bookName;
+
+
+}
---
+++ ReaderServer/library/src/main/java/com/giants3/reader/entity/ComicChapterFile.java
@@ -0,0 +1,25 @@
+package com.giants3.reader.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by davidleen29 on 2018/5/13.
+ */
+@Entity(name = "T_ComicChapterFile")
+public class ComicChapterFile {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+    public long chapterId;
+    public long bookId;
+    public int  index;
+    public String url;
+    public int width;
+    public int height;
+
+}
